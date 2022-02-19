7714c597aa7d1c929f33169b67a8c570763a6750
suguru <sgr.inoue@gmail.com>
2022-02-19 23:21:02 +0100

    serve static files

--- src/hooks/useRemoteWords.ts
+++ src/hooks/useRemoteWords.ts
@@ -4,7 +4,7 @@ import { Action } from "../models/State";
 export default function useRemoteWords(dispatch: Dispatch<Action>) {
   useEffect(() => {
     (async () => {
-      const res = await fetch("http://localhost:8080/words");
+      const res = await fetch(`${process.env.REACT_APP_API_ROOT}/words`);
 
       if (res.status !== 200) {
         window.alert("Could not load data");
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { loadWords, fetchFromWiktionary, addWord } from "./dictionary";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT ?? 8080;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/words", (req, res) => {
+app.get("/api/words", (req, res) => {
   res.json(loadWords());
 });
 
-app.get("/words/:word", async (req, res) => {
+app.get("/api/words/:word", async (req, res) => {
   if (loadWords().some((w) => w.german === req.params.word)) {
     return res.status(400).end();
   }
