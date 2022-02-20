b392fa51d02ef26df6fa51877841c3a676b4e624
suguru <sgr.inoue@gmail.com>
2022-02-20 11:47:32 +0100

    migrate meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import "./index.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Yet another flashcard app for learning the German language"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Lernkartei v2</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
