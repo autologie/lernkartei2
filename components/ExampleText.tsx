14464a182beb5bf043bfde865039c5b5cca1e51c
suguru <sgr.inoue@gmail.com>
2022-02-20 20:17:57 +0100

    fix: replaceAll not available in node

--- components/ExampleText.tsx
+++ components/ExampleText.tsx
@@ -1,4 +1,47 @@
-import { Fragment, ReactNode } from "react";
+import { Fragment } from "react";
+
+function collectItems<T>(
+  children: string,
+  factory: (
+    segment: string,
+    kind: "tail" | "match" | "pre" | "symbol",
+    key: number
+  ) => T
+): T[] {
+  const regex = /\[\[[^\].,?!]+(,|\?|.|!)?\]\]/g;
+  const segments: T[] = [];
+  let index = 0;
+
+  while (true) {
+    const matched = regex.exec(children);
+
+    if (matched === null) {
+      segments.push(
+        factory(children.slice(index, children.length), "tail", 3 * index)
+      );
+      break;
+    }
+
+    const nextIndex = matched.index + matched[0].length;
+
+    segments.push(
+      factory(children.slice(index, matched.index), "pre", 3 * index),
+      factory(
+        children.slice(
+          matched.index + 2,
+          nextIndex - 2 - (matched[1]?.length ?? 0)
+        ),
+        "match",
+        3 * index + 1
+      ),
+      factory(matched[1] ?? "", "symbol", 3 * index + 2)
+    );
+
+    index = nextIndex;
+  }
+
+  return segments;
+}
 
 export default function ExampleText({
   children: children_,
@@ -11,55 +54,35 @@ export default function ExampleText({
     .replace(/{[^}]*}/, "")
     .replace(/\[\[mit (Genitiv|Dativ|Akkusativ|Nominativ):\]\]/, "")
     .trim(); // remove math equation notation
-  const regex = /\[\[[^\].,?!]+(,|\?|.|!)?\]\]/g;
 
   if (mode === "mask") {
     return (
       <>
-        {children.replaceAll(regex, (_, symbol) => {
-          return `_____${symbol === undefined ? "" : ` ${symbol}`}`;
-        })}
+        {collectItems(children, (segment, kind) =>
+          kind === "match"
+            ? "_____"
+            : kind === "symbol"
+            ? ` ${segment}`
+            : segment
+        ).join("")}
       </>
     );
   }
 
-  const segments: ReactNode[] = [];
-  let index = 0;
-
-  while (true) {
-    const matched = regex.exec(children);
-
-    if (matched === null) {
-      segments.push(
-        <Fragment key={index * 3}>
-          {children.slice(index, children.length)}
-        </Fragment>
-      );
-      break;
-    }
-
-    const nextIndex = matched.index + matched[0].length;
-
-    segments.push(
-      <Fragment key={index * 3}>
-        {children.slice(index, matched.index)}
-      </Fragment>,
-      <i
-        key={index * 3 + 1}
-        className={mode === "italic-green" ? "text-green-600" : ""}
-      >
-        {children.slice(
-          matched.index + 2,
-          nextIndex - 2 - (matched[1]?.length ?? 0)
-        )}
-      </i>,
-      <Fragment key={index * 3 + 2}>
-        {matched[1] === undefined ? "" : matched[1]}
-      </Fragment>
-    );
-
-    index = nextIndex;
-  }
-
-  return <>{segments}</>;
+  return (
+    <>
+      {collectItems(children, (segment, kind, key) =>
+        kind === "match" ? (
+          <i
+            key={key}
+            className={mode === "italic-green" ? "text-green-600" : ""}
+          >
+            {segment}
+          </i>
+        ) : (
+          <Fragment key={key}>{segment}</Fragment>
+        )
+      )}
+    </>
+  );
 }
