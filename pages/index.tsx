3a9a657db1e6e5aa222183d1147bc97caaa3d9db
suguru <sgr.inoue@gmail.com>
2022-02-25 12:23:42 +0100

    fix session restoration

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
 export async function getServerSideProps(
   ctx: GetServerSidePropsContext
 ): Promise<GetServerSidePropsResult<{}>> {
-  const sessionId = !(ctx.req.cookies.sessionId ?? "").match(/^[\d]{6,6}$/)
+  const sessionId = !(ctx.req.cookies.sessionId ?? "").match(/^[\d]{8,8}$/)
     ? Math.random().toString().slice(2, 10)
     : ctx.req.cookies.sessionId;
 
